450060be25e4124633051762ba1036103ea26a63
Jonathan <jonathan.plastivene@outlook.com>
2025-01-14 22:52:35 +0100

    init static

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,8 +138,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS =  [BASE_DIR / 'static']
 
 # prod
-#STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 # Default primary key field type
